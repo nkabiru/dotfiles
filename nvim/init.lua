5cb47a4c5cafaea700f25dd35e8d5694e291abcc
Njenga Kabiru <njengakabiru@gmail.com>
2023-02-27 12:51:45 +0300

    Add neovim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,3 @@
+require('user/plugins')
 require('user/options')
+require('user/keymaps')
--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -25,7 +25,7 @@ vim.keymap.set('i', ';;', '<Esc>A;')
 vim.keymap.set('i', ',,', '<Esc>A,')
 
 -- Quickly clear search highlighting
-vim.keymap.set('n', '<Leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<Leader>k', ':nohlsearch<CR>')
 
 -- Move lines up and down.
 vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
