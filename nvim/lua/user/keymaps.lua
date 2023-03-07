348d9f1debdfcce59dd37e5ebaaa921e38adc8b6
Njenga Kabiru <njengakabiru@gmail.com>
2023-03-07 10:32:10 +0300

    Keep plugins updated according to source files

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,20 +1,20 @@
--- Space is my leader
+-- Space is my leader.
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Move by terminal rows, not lines when text is wrapped, unless a count is provided
+-- When text is wrapped, move by terminal rows, not lines, unless a count is provided.
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
--- Reselect visual selection after indenting
+-- Reselect visual selection after indenting.
 vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
 
--- Maintain cursor position when yanking a visual selection
+-- Maintain the cursor position when yanking a visual selection.
 -- http://ddrscott.github.io/blog/2016/yank-without-jank/
 vim.keymap.set('v', 'y', 'myy`y')
 
--- Disable annoying command line typo
+-- Disable annoying command line typo.
 vim.keymap.set('n', 'q:', ':q')
 
 -- Paste replace visual selection without copying it.
@@ -24,8 +24,11 @@ vim.keymap.set('v', 'p', '"_dP')
 vim.keymap.set('i', ';;', '<Esc>A;')
 vim.keymap.set('i', ',,', '<Esc>A,')
 
--- Quickly clear search highlighting
+-- Quickly clear search highlighting.
 vim.keymap.set('n', '<Leader>k', ':nohlsearch<CR>')
+
+-- Open the current file in the default program (on Mac this should just be just `open`).
+vim.keymap.set('n', '<Leader>x', ':!xdg-open %<CR><CR>')
 
 -- Move lines up and down.
 vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -10,10 +10,10 @@ vim.opt.wrap = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.wildmode = 'longest:full,full'
+vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
 
 vim.opt.title = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = 'a' -- enable mouse for all modes
 
 vim.opt.termguicolors = true
 
@@ -22,9 +22,9 @@ vim.opt.spell = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.list = true
+vim.opt.list = true -- enable the below listchars
 vim.opt.listchars = { tab = '▸ ', trail = '·' }
-vim.opt.fillchars:append({ eob = ' ' })
+vim.opt.fillchars:append({ eob = ' ' }) -- remove the ~ from end of buffer
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
@@ -32,9 +32,12 @@ vim.opt.splitright = true
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = 'unnamedplus' -- Use system clipboard
 
-vim.opt.confirm = true
-vim.opt.undofile = true
-vim.opt.backup = true
-vim.opt.backupdir:remove('.')
+vim.opt.confirm = true -- ask for confirmation instead of erroring
+
+vim.opt.signcolumn = 'yes:1'
+
+vim.opt.undofile = true -- persistent undo
+vim.opt.backup = true -- automatically save a backup file
+vim.opt.backupdir:remove('.') -- keep backups out of the current directory
