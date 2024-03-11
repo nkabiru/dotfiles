6b10bd4734a6605b3c96a19b3c8c35079669323a
nkabiru <njengakabiru@gmail.com>
2024-03-11 18:47:47 +0300

    Update chadrc & add kitty.conf

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,10 +1,21 @@
 local M = {}
 
 M.ui = {
-  theme = "tokyonight",
-  statusline = {
-    separator_style = "block"
-  }
+	theme = "tokyonight",
+	statusline = {
+		separator_style = "block",
+	},
+
+	nvdash = {
+		load_on_startup = true,
+		header = {
+			"   ░░░    ░░    ░░   ░░      ",
+			"   ▒▒▒▒   ▒▒    ▒▒  ▒▒       ",
+			"   ▒▒ ▒▒  ▒▒    ▒▒▒▒▒        ",
+			"   ▓▓  ▓▓ ▓▓    ▓▓  ▓▓       ",
+			"   ██   ████ ██ ██   ██ ██   ",
+		},
+	},
 }
 
 return M
